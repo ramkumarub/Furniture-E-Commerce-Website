dcd6a73e8104f370b0287ef576eb8111c841baaa
Ram Kumar U B <ramkumarbalakumar2004@gmail.com>
2026-05-20 10:55:25 +0530

    Changes

--- server/server.js
+++ server/server.js
@@ -2,15 +2,6 @@ const dotenv = require("dotenv").config()
 const app = require("./app")
 const connectDB =  require("./config/db")
 
-// app.listen(process.env.PORT, async() => {
-//     try {
-//         await connectDB()
-//         console.log("Server Connected Successfully")
-//     }
-//     catch (error) {
-//         console.log(error)
-//     }
-// })
 const startServer = async() =>{
     try {
         await connectDB()
